94ad972b8fd5a9a8a8b5689106fc4cfff3442809
keatonconrad <keaton.conrad@gmail.com>
2021-08-06 15:40:04 -0500

    Added tests for scoring and clicking on an already-played cell

--- test/unit/specs/Board.spec.js
+++ test/unit/specs/Board.spec.js
@@ -6,6 +6,7 @@ describe('Board.vue', () => {
   it('should create correct size board', () => {
     const wrapper = mount(Board);
     expect(wrapper.vm.$data.board.length).toEqual(8);
+    expect(wrapper.vm.$data.board[0].length).toEqual(8);
   })
 
   it('should change turns when a cell is clicked', () => {
@@ -13,5 +14,23 @@ describe('Board.vue', () => {
     expect(wrapper.vm.$data.turn).toEqual('black');
     wrapper.find('td').trigger('click');
     expect(wrapper.vm.$data.turn).toEqual('white');
+  })
+
+  it('should not do anything when clicking on an already-played cell', () => {
+    const wrapper = mount(Board);
+    expect(wrapper.vm.$data.turn).toEqual('black');
+    wrapper.find('td').trigger('click');
+    wrapper.find('td').trigger('click');
+    expect(wrapper.vm.$data.turn).toEqual('white');
+  })
+
+  it('should return correct scores', () => {
+    const wrapper = mount(Board);
+    const cells = wrapper.findAll('td');
+    for (var i = 0; i <= 4; i++) {
+      cells.at(i).trigger('click');
+    }
+    expect(wrapper.vm.$data.scores.black).toEqual(3);
+    expect(wrapper.vm.$data.scores.white).toEqual(2);
   })
 })
