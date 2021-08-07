baba8adef9ae58f3889e063bab4665521ce7e088
keatonconrad <keaton.conrad@gmail.com>
2021-08-07 12:01:29 -0500

    Added capturing tests; refactored Board and App tests

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -2,17 +2,20 @@ import { mount } from '@vue/test-utils';
 import App from '@/App.vue';
 
 describe('Board.vue', () => {
-  it('allows setting board dimensions', () => {
-    const wrapper = mount(App);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(App);
     jest.spyOn(window, 'alert').mockReturnValue();
+  });
+
+  it('allows setting board dimensions', () => {
     wrapper.find('input').setValue(8);
     wrapper.find('button').trigger('click');
     expect(window.alert).not.toHaveBeenCalled();
   });
 
   it('rejects board size under 3', () => {
-    const wrapper = mount(App);
-    jest.spyOn(window, 'alert').mockReturnValue();
     wrapper.find('input').setValue(2);
     wrapper.find('button').trigger('click');
     expect(window.alert).toHaveBeenCalled();
--- test/unit/specs/Board.spec.js
+++ test/unit/specs/Board.spec.js
@@ -2,64 +2,88 @@ import { mount } from '@vue/test-utils';
 import Board from '@/components/Board.vue';
 
 describe('Board.vue', () => {
-  it('should create correct size board', () => {
-    const wrapper = mount(Board, {
+  let wrapper, cells;
+
+  beforeEach(() => {
+    wrapper = mount(Board, {
       propsData: {
         size: 8
       }
     });
+    cells = wrapper.findAll('td');
+  });
+
+  it('should create correct size board', () => {
     expect(wrapper.vm.$data.board.length).toEqual(8);
     expect(wrapper.vm.$data.board[0].length).toEqual(8);
   });
 
   it('should change turns when a cell is clicked', () => {
-    const wrapper = mount(Board, {
-      propsData: {
-        size: 8
-      }
-    });
     expect(wrapper.vm.$data.turn).toEqual('black');
-    wrapper.find('td').trigger('click');
+    cells.at(0).trigger('click');
     expect(wrapper.vm.$data.turn).toEqual('white');
   });
 
   it('should not do anything when clicking on an already-played cell', () => {
-    const wrapper = mount(Board, {
-      propsData: {
-        size: 8
-      }
-    });
     expect(wrapper.vm.$data.turn).toEqual('black');
-    wrapper.find('td').trigger('click');
-    wrapper.find('td').trigger('click');
+    cells.at(0).trigger('click');
+    cells.at(0).trigger('click');
     expect(wrapper.vm.$data.turn).toEqual('white');
   });
 
   it('should return correct scores', () => {
-    const wrapper = mount(Board, {
-      propsData: {
-        size: 5
-      }
-    });
-    const cells = wrapper.findAll('td');
-    for (var i = 0; i < cells.length; i++) {
+    for (let i = 0; i < cells.length; i++) {
       cells.at(i).trigger('click');
     }
-    expect(wrapper.vm.$data.scores.black).toEqual(21);
-    expect(wrapper.vm.$data.scores.white).toEqual(4);
+    expect(wrapper.vm.$data.scores.black).toEqual(56);
+    expect(wrapper.vm.$data.scores.white).toEqual(8);
   });
 
   it('should end game when all spaces are full', () => {
-    const wrapper = mount(Board, {
-      propsData: {
-        size: 8
-      }
-    });
-    const cells = wrapper.findAll('td');
-    for (var i = 0; i < cells.length; i++) {
+    for (let i = 0; i < cells.length; i++) {
       cells.at(i).trigger('click');
     }
     expect(wrapper.vm.$data.totalCells).toEqual(64);
     expect(wrapper.vm.$data.gameOver).toEqual(true);
+  });
+
+  it('should capture a white circle to the left', () => {
+    for (let cell in [0, 1, 2]) {
+      cells.at(cell).trigger('click');
+    }
+    expect(wrapper.vm.$data.scores.black).toEqual(3);
+    expect(wrapper.vm.$data.scores.white).toEqual(0);
+  });
+
+  it('should capture two white circles to the left', () => {
+    for (let cell in [0, 1, 4, 2, 3]) {
+      cells.at(cell).trigger('click');
+    }
+    expect(wrapper.vm.$data.scores.black).toEqual(5);
+    expect(wrapper.vm.$data.scores.white).toEqual(0);
+  });
+
+  it('should capture a white circle to the right', () => {
+    for (let cell in [2, 1, 0]) {
+      cells.at(cell).trigger('click');
+    }
+    expect(wrapper.vm.$data.scores.black).toEqual(3);
+    expect(wrapper.vm.$data.scores.white).toEqual(0);
+  });
+
+  it('should capture a white circle looking upward', () => {
+    for (let cell in [0, 8, 16]) {
+      cells.at(cell).trigger('click');
+    }
+    expect(wrapper.vm.$data.scores.black).toEqual(3);
+    expect(wrapper.vm.$data.scores.white).toEqual(0);
+  });
+
+  it('should capture a white circle looking downward', () => {
+    for (let cell in [16, 8, 0]) {
+      cells.at(cell).trigger('click');
+    }
+    expect(wrapper.vm.$data.scores.black).toEqual(3);
+    expect(wrapper.vm.$data.scores.white).toEqual(0);
   });
 });
